78183d4e7ccce5dba5726e33d48d8db7188c158e
Ahmad Beni Rusli <159234974+ahmdbnrsl@users.noreply.github.com>
2025-11-07 14:54:18 +0700

    Modify layout metadata for SIAKAD
    Updated metadata for SIAKAD with new title, description, and Open Graph settings.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,33 @@ const geistSans = Outfit({
 });
 
 export const metadata: Metadata = {
-	title: 'SIAKAD',
-	description: 'Sistem Informasi Akademik',
+  title: "SIAKAD | UNIKMA",
+  description: "Sistem Informasi Akademik",
+  metadataBase: new URL("https://beni-basisdata-uts.ab-rust.xyz"),
+
+  openGraph: {
+    title: "SIAKAD | UNIKMA",
+    description: "Sistem Informasi Akademik | Universitas Komputama",
+    url: "https://beni-basisdata-uts.ab-rust.xyz",
+    siteName: "SIAKAD | UNIKMA",
+    images: [
+      {
+        url: "https://cdn.ab-rust.xyz/file/1762501538955.png",
+        width: 1200,
+        height: 630,
+        alt: "SIAKAD Thumbnail",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "SIAKAD | UNIKMA",
+    description: "Sistem Informasi Akademik | Universitas Komputama",
+    images: ["https://cdn.ab-rust.xyz/file/1762501538955.png"],
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +45,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<meta name="theme-color" content="#0f172a" />
+			</head>
 			<body className={`${geistSans.variable} antialiased`}>
 				<Container>{children}</Container>
 			</body>
